15f482257a1e0b4b059cb98a970a68ece634ca87
Araya <joaoaraya7@gmail.com>
2021-09-29 21:54:57 -0300

    estados nos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { ButtonA, ButtonB } from './components/button'
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Olá, mundo!</h1>
       <ButtonA text="Clique em mim" />
       <ButtonB />
-      <ButtonB />
       <ButtonA text="Não clique em mim" />
+      <ButtonB />
+      <ButtonA text="Clique em mim (se quiser)" />
+      <ButtonB />
     </div>
   );
 }
